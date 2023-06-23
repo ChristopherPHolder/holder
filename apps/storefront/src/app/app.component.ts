826aa72c7c6a134cfc08fb7a8865b939bd7b397c
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-23 10:23:49 +0200

    feat: change name to storefront

--- apps/storefront/src/app/app.component.ts
+++ apps/storefront/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'holder-root',
+  selector: 'storefront-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
