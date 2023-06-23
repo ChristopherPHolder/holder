fa7aae5596b099e92120ca650d49687adb10d1dd
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-24 01:16:02 +0200

    feat: change detection onpush

--- libs/ui/src/lib/component/throbbler/throbber.component.ts
+++ libs/ui/src/lib/component/throbbler/throbber.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ui-throbbler',
@@ -6,6 +6,7 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   templateUrl: './throbber.component.html',
   styleUrls: ['./throbber.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThrobberComponent {
   @Input() visible = false;
