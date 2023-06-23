8878d1a6ab1c5c57a446e60fb11846fcc9b25873
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-23 09:50:58 +0200

    feat: add storefront project

---
+++ apps/storefront/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'holder-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'storefront';
+}
