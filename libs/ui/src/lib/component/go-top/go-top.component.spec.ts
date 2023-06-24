e0a3e5b223a4e5ceabc4bd9c517dd7bcaf83936a
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-24 23:37:39 +0200

    refactor: fix linter

--- libs/ui/src/lib/component/go-top/go-top.component.spec.ts
+++ libs/ui/src/lib/component/go-top/go-top.component.spec.ts
@@ -2,20 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GoTopComponent } from './go-top.component';
 
 describe('GoTopComponent', () => {
-  let component: GoTopComponent;
-  let fixture: ComponentFixture<GoTopComponent>;
+	let component: GoTopComponent;
+	let fixture: ComponentFixture<GoTopComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GoTopComponent],
-    }).compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [GoTopComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(GoTopComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(GoTopComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
