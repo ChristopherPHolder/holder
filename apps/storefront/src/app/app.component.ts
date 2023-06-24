585d4cda5c74e37cd918341df57d66ded1d9a1af
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-24 22:38:55 +0200

    refactor: convert location handling to input

--- apps/storefront/src/app/app.component.ts
+++ apps/storefront/src/app/app.component.ts
@@ -5,22 +5,59 @@ import {
 	NavigationStart,
 	Router,
 } from '@angular/router';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map, shareReplay, tap } from 'rxjs/operators';
 import * as $ from 'jquery';
 
+type NavigationEvents = NavigationStart | NavigationCancel | NavigationEnd;
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	public navigating$ = inject(Router).events.pipe(
+	private router = inject(Router);
+	private navigating$ = this.router.events.pipe(
 		filter(
 			(event) =>
-				event instanceof (NavigationStart || NavigationCancel || NavigationEnd)
+				event instanceof NavigationStart ||
+				event instanceof NavigationCancel ||
+				event instanceof NavigationEnd
 		),
-		map((event) => !(event instanceof NavigationStart)),
-		tap((navEnded) => navEnded || window.scrollTo(0, 0)),
-		tap((navEnded) => navEnded || $.getScript('../assets/js/main.js'))
+		map((event) => event as NavigationEvents),
+		tap((e) => console.log('e', e)),
+		tap(
+			(event) =>
+				event instanceof NavigationEnd ||
+				window.scrollTo(0, 0) ||
+				$.getScript('../assets/js/main.js')
+		),
+		shareReplay()
+	);
+
+	public throbblerVisible$ = this.navigating$.pipe(
+		map(
+			(event) =>
+				!(event instanceof NavigationEnd || event instanceof NavigationCancel)
+		)
+	);
+
+	public footerBgClass$ = this.navigating$.pipe(
+		filter(
+			(event) =>
+				event instanceof NavigationEnd || event instanceof NavigationCancel
+		),
+		map(() => this.router.url),
+		filter(
+			(url) =>
+				![
+					'/home-two',
+					'/home-four',
+					'/services-details',
+					'/events-details',
+					'/courses',
+					'/courses-details',
+				].includes(url)
+		),
+		map((url) => (url === '/about-us' ? 'bg-fffaf3' : 'bg-color'))
 	);
 }
