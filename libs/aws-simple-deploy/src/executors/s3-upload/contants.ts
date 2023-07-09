58d87b7668b2ae0aa9432b3f37bd2ea4213fa055
ChristopherPHolder <toholderandrews@gmail.com>
2023-07-09 14:59:10 +0200

    feat: extend deploy

--- libs/aws-simple-deploy/src/executors/s3-upload/contants.ts
+++ libs/aws-simple-deploy/src/executors/s3-upload/contants.ts
@@ -1,2 +1,3 @@
 export const ERROR_NO_BUCKET = 'Error: S3Upload requires a bucket to be passed in options';
 export const ERROR_NO_BUILD_DIR = 'Error: S3Upload requires a buildDir to be passed in options';
+export const ERROR_NO_FILES_IN_BUILD_DIR = 'Error: no files where found in the build Dir';
--- libs/aws-simple-deploy/src/executors/s3-upload/executor.ts
+++ libs/aws-simple-deploy/src/executors/s3-upload/executor.ts
@@ -1,10 +1,10 @@
-import { readdirSync } from 'fs';
-import { join } from 'path';
+import { existsSync, readdirSync, readFileSync } from 'fs';
+import { join, posix } from 'path';
 import { ExecutorContext } from '@nx/devkit';
 
 import { S3UploadExecutorSchema } from './schema';
-import { ERROR_NO_BUCKET, ERROR_NO_BUILD_DIR } from './contants';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { ERROR_NO_BUCKET, ERROR_NO_BUILD_DIR, ERROR_NO_FILES_IN_BUILD_DIR } from './contants';
+import { PutObjectCommand, PutObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
 
 
 export default async function runExecutor(
@@ -26,31 +26,33 @@ export default async function runExecutor(
     return { success: false, message: ERROR_NO_BUILD_DIR };
   }
 
-  try {
-    const files = getFileNames(options.buildDir);
-    console.log(files)
-  } catch (e) {
-    console.error(e)
+  const files = getFileNames(options.buildDir);
+  if (!files.length) {
+    console.error(ERROR_NO_FILES_IN_BUILD_DIR);
+    return { success: false, message: ERROR_NO_FILES_IN_BUILD_DIR };
   }
 
   const client = new S3Client({
     region: 'us-east-1'
   });
 
-  const command = new PutObjectCommand({
-    Bucket: options.bucket,
-    Key: undefined,
-    Body: 'Hello world!'
-  });
+  const commandOutputs: Promise<PutObjectCommandOutput>[] = files.map((file) => {
+    context.isVerbose && console.info(`Uploading: ${file.replace(options.buildDir, '')}`);
+    const command = new PutObjectCommand( {
+      Key: file.replace(options.buildDir, ''),
+      Bucket: options.bucket,
+      Body: readFileSync(file),
+      ContentType: contentTypeMap[fileExt(file)],
+      CacheControl: cachecontrolMap[fileExt(file)]
+    });
+    return client.send(command);
+  })
 
-  try {
-    const response = await client.send(command);
-    console.log(response);
-  } catch (err) {
-    console.error(err);
-  }
+  await Promise.all(commandOutputs);
 
   client.destroy();
+
+  context.isVerbose && console.info(`Successfully uploaded ${files.length} to ${options.bucket}`);
 
 	return {
 		success: true,
@@ -59,7 +61,22 @@ export default async function runExecutor(
 }
 
 function getFileNames(distDir: string): string[] {
+  if (!existsSync(distDir)) return [];
   return readdirSync(distDir, {encoding: 'utf-8', withFileTypes: true}).flatMap(entry => {
-    return entry.isFile() ? join(distDir, entry.name) : getFileNames(join(distDir, entry.name))
+    return entry.isFile() ? posix.join(distDir, entry.name) : getFileNames( posix.join(distDir, entry.name))
   })
+}
+
+const fileExt = (fileName: string) => fileName.substring(fileName.lastIndexOf('.') + 1, fileName.length) || fileName;
+
+type FileType = 'html' | 'js';
+
+const contentTypeMap: Record<FileType, string> = {
+  'html': 'text/html',
+  'js': 'application/javascript'
+}
+
+const cachecontrolMap: Record<FileType, string> = {
+  'html': 'text/html',
+  'js': 'public, max-age=31536000, immutable'
 }
