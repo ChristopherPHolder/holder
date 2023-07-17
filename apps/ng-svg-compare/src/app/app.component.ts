6e576fb566ccbff8f5e73d5ff0728b4ddce06066
ChristopherPHolder <toholderandrews@gmail.com>
2023-07-17 07:53:18 +0200

    feat: init ng svg compare

--- apps/ng-svg-compare/src/app/app.component.ts
+++ apps/ng-svg-compare/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
 	standalone: true,
-	imports: [RouterModule],
+  imports: [RouterModule, NgForOf],
 	selector: 'holder-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 	title = 'ng-svg-compare';
+
+  navItems = [
+    { title: 'ngx-fast-svg' },
+    { title: 'ngx-fast-svg' },
+    { title: 'ngx-fast-svg' },
+    { title: 'ngx-fast-svg' },
+    { title: 'ngx-fast-svg' }
+  ]
 }
